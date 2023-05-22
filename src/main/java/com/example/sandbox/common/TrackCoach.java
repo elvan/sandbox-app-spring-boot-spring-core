e6f3d9d86113749235cdb08e0643a0fa4682dce1
Elvan Hidayat <elvanhd@gmail.com>
2023-05-22 18:43:59 +0700

    bean scopes

--- src/main/java/com/example/sandbox/common/TrackCoach.java
+++ src/main/java/com/example/sandbox/common/TrackCoach.java
@@ -1,10 +1,8 @@
 package com.example.sandbox.common;
 
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy
 public class TrackCoach implements Coach {
     public TrackCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
