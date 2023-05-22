99b7d8fde21936f14661780879421ab3d146debd
Elvan Hidayat <elvanhd@gmail.com>
2023-05-22 14:59:42 +0700

    qualifiers

--- src/main/java/com/example/sandbox/rest/DemoController.java
+++ src/main/java/com/example/sandbox/rest/DemoController.java
@@ -2,7 +2,7 @@ package com.example.sandbox.rest;
 
 import com.example.sandbox.common.Coach;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +12,7 @@ public class DemoController {
     // define a private field for the dependency
     private Coach myCoach;
 
-    @Autowired
-    public void setCoach(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
