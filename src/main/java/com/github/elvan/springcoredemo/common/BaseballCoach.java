83a0391b7191828c041bff30e99b46c312778c85
Elvan Hidayat <elvanhd@gmail.com>
2024-11-10 23:55:19 +0700

    Add new Coach implementations and configure DemoController
    - Create BaseballCoach, TennisCoach, and TrackCoach classes with workouts
    - Annotate each new Coach implementation with @Component for injection
    - Update DemoController to use constructor injection with @Qualifier
    - Set default injected Coach as CricketCoach for DemoController
    - Enhance dependency flexibility by adding multiple Coach options

---
+++ src/main/java/com/github/elvan/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,13 @@
+package com.github.elvan.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+  @Override
+  public String getDailyWorkout() {
+    return "Spend 30 minutes in batting practice";
+  }
+
+}
--- src/main/java/com/github/elvan/springcoredemo/rest/DemoController.java
+++ src/main/java/com/github/elvan/springcoredemo/rest/DemoController.java
@@ -1,7 +1,7 @@
 package com.github.elvan.springcoredemo.rest;
 
 import com.github.elvan.springcoredemo.common.Coach;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +11,7 @@ public class DemoController {
   // define a private field for the dependency
   private Coach myCoach;
 
-  @Autowired
-  public void setCoach(Coach theCoach) {
+  public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
     myCoach = theCoach;
   }
 
