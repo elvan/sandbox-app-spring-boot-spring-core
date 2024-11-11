299c7bebd1be9b3367dc1500859090e27a705c5d
Elvan Hidayat <elvanhd@gmail.com>
2024-11-11 08:13:07 +0700

    Set CricketCoach to prototype scope and add bean comparison
    - Mark CricketCoach with prototype scope for non-singleton behavior
    - Update DemoController to inject two instances of CricketCoach
    - Add /check endpoint to compare injected CricketCoach beans
    - Remove lazy initialization setting from application properties

--- src/main/java/com/github/elvan/springcoredemo/rest/DemoController.java
+++ src/main/java/com/github/elvan/springcoredemo/rest/DemoController.java
@@ -10,15 +10,24 @@ public class DemoController {
 
   // define a private field for the dependency
   private Coach myCoach;
+  private Coach anotherCoach;
 
-  public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+  public DemoController(
+      @Qualifier("cricketCoach") Coach theCoach,
+      @Qualifier("cricketCoach") Coach theAnotherCoach) {
     System.out.println("In constructor: " + getClass().getSimpleName());
     myCoach = theCoach;
+    anotherCoach = theAnotherCoach;
   }
 
   @GetMapping("/dailyworkout")
   public String getDailyWorkout() {
     return myCoach.getDailyWorkout();
+  }
+
+  @GetMapping("/check")
+  public String check() {
+    return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
   }
 
 }
